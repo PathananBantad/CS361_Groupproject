24719c5981158845782b422f65fdef3eb10bebca
mymyne <baby.mxixxlxd@gmail.com>
2026-08-28 23:44:32 +0700

    version 1 firstpage

---
+++ script.js
@@ -0,0 +1,10 @@
+const reveals = document.querySelectorAll('.reveal');
+const io = new IntersectionObserver((entries) => {
+  entries.forEach(e => {
+    if (e.isIntersecting) {
+      e.target.classList.add('in');
+      io.unobserve(e.target);
+    }
+  });
+}, { threshold: .12 });
+reveals.forEach(el => io.observe(el));
